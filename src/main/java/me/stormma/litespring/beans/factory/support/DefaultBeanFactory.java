3db9cc8b32f3607b38ca2951d92300f4f636e59e
StormMa <StormMaybin@gmail.com>
2018-07-09 14:48:12 +0800

    add gitignore

--- src/main/java/me/stormma/litespring/beans/factory/support/DefaultBeanFactory.java
+++ src/main/java/me/stormma/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -12,8 +12,11 @@ public class DefaultBeanFactory implements BeanFactory {
 
     }
 
-
     public BeanDefinition getBeanDefinition(String beanId) {
+        return null;
+    }
+
+    public Object getBean(String beanId) {
         return null;
     }
 }
